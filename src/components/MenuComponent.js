2c1b6f96091dc543e70aa60159a41658018ec11f
Mehul Porwal <64722605+mehulgithub-1903@users.noreply.github.com>
2020-07-10 19:02:32 +0530

    React Components Part 2

---
+++ src/components/MenuComponent.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody,
+  CardTitle } from 'reactstrap';
+
+class Menu extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            
+        };
+    }
+
+    render() {
+        const menu = this.props.dishes.map((dish) => {
+            return (
+              <div key={dish.id} className="col-12 col-md-5 m-1">
+                <Card  tag="li">
+                      <CardImg width="100%" src={dish.image} alt={dish.name} />
+                      <CardImgOverlay>
+                      <CardTitle>{dish.name}</CardTitle>
+                      </CardImgOverlay>
+                </Card>
+              </div>
+            );
+        });
+
+        return (
+          <div className="container">
+            <div className="row">
+            
+                  {menu}
+              
+            </div>
+          </div>
+        );
+    }
+}
+
+export default Menu;
